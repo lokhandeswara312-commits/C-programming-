78abbda014278b668fed6992c272fb6cce462e8a
lokhandeswara312-commits <lokhandeswara312@eng.rizvi.edu.in>
2025-10-06 16:19:14 +0530

    Add files via upload

---
+++ prog_1.c
@@ -0,0 +1,26 @@
+
+#include <stdio.h>
+int main()
+{
+    int age;
+    printf("Enter age :");
+    scanf("%d",&age);
+    printf("Age is %d \n",age);
+
+    float Pi=3.14159;
+    printf("PI is:%.5f\n",Pi);
+
+    char alphabet='A';
+    printf("Alphabet is : %c\n",alphabet);
+
+    char name[]="Rizvi collage";
+    printf("Name is :%s",name);
+
+
+
+    //intf("hello world \n");
+    //printf("Aaradhya Korgaonkar");
+
+
+    return 0;
+}
