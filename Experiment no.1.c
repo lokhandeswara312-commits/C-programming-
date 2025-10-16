bc7fa65815f3d260b8273bc4999dbe9911ac9d22
lokhandeswara312-commits <lokhandeswara312@eng.rizvi.edu.in>
2025-10-16 20:17:13 +0530

    Add files via upload

---
+++ Experiment no.1.c
@@ -0,0 +1,17 @@
+/*Name: Swara Lokhande
+Class: F.E (Civil)
+UIN: 251C043
+DIV:B 
+Roll no: 30*/
+
+#include <stdio.h> 
+
+int main()
+{
+    printf("Name:\t\tSwara Lokhande\n");
+    printf("Age:\t\t17\n");
+    printf("Class:\t\tF.E(Civil Engineering\n");
+    printf("Division:\tB\n");
+    printf("UIN:\t\t251C043");
+    return 0;
+}
