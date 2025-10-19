11d46d097708c1a4d7d17477c12df1b23c83b024
Swara Lokhande <lokhandeswara312@eng.rizvi.edu.in>
2025-10-19 12:43:07 +0530

    Add files via upload

---
+++ Experiment no.2(Task1).c
@@ -0,0 +1,25 @@
+/*Name: Swara Lokhande
+Class: F.E(Civil Engineering)
+UIN:251C043
+DIV: B 
+Roll no. 30*/
+
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    
+    //Input number
+    printf("Enter a number");
+    scanf("%d", &num);
+    
+    //Check even or odd
+if (num %2 == 0)
+    printf("%d is an even number.\n", num);
+ 
+ else 
+    printf("%d is an odd number.\n", num);
+
+    return 0;
+}
