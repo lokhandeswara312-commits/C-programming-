3a4924a017bb0059bb7aaa64aad5a774f09ee7b1
Swara Lokhande <lokhandeswara312@eng.rizvi.edu.in>
2025-12-09 09:19:41 +0530

    Add files via upload

---
+++ Experiment no.8(Task 2).c
@@ -0,0 +1,31 @@
+/*Name:- Swara Lokhande
+  Class:- F.E(Civil Engineering
+  UIN:- 251C043
+  Div:- B*
+  Roll no:- 30*/ 
+  
+  #include <stdio.h>
+  #include <string.h>
+  
+  int main() {
+      char str[100];
+      int i, len, flag = 0;
+      
+      printf("Enter a string: ");
+      gets(str);
+      len = strlen(str);
+      
+      for (i = 0; i < len / 2; i++) {
+          if (str[i] ! = str[len -i -1]) {
+          flag = 1;
+          break; 
+      }   
+    }   
+    
+    if (flag == 0)
+        printf("The string is a Palindrome.\n");
+    else
+        printf("The string is not Palindrone.\n");
+        
+    return 0;
+}
